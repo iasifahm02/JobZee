c72c91ba5cfb4fd40a50395108e41ba44bbc11f3
iasifahm02 <iasif.ahm02@gmail.com>
2024-05-31 00:54:46 +0530

    Salary Based Filtering UI done

--- JobZee-client/src/Sidebar/Salary.jsx
+++ JobZee-client/src/Sidebar/Salary.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import Button from './Button'
+import InputField from '../Components/InputField'
 
-const Salary = ({handleChange, handleClick}) => {
+const Salary = ({ handleChange, handleClick }) => {
   return (
     <div>
-        <h4 className='text-lg font-medium mb-2'>Salary</h4>
-        <div className='mb-4'>
-            <Button onClickHandler={handleClick} value="" title="Hourly"></Button>
-            <Button onClickHandler={handleClick} value="Monthly" title="Monthly"></Button>
-            <Button onClickHandler={handleClick} value="Yearly" title="Yearly"></Button>
-        </div>
+      <h4 className='text-lg font-medium mb-2'>Salary</h4>
+      <div className='mb-4'>
+        <Button onClickHandler={handleClick} value="" title="Hourly"></Button>
+        <Button onClickHandler={handleClick} value="Monthly" title="Monthly"></Button>
+        <Button onClickHandler={handleClick} value="Yearly" title="Yearly"></Button>
+      </div>
+      {/* For radio button */}
+      <div>
+        <label className='sidebar-label-container'>
+          <input type="radio" name="test" value="" onChange={handleChange} />
+          <span className='checkmark'></span> Any
+        </label>
+        {/* Now We'll create component for all location option */}
+        <InputField handleChange={handleChange} value="30" title="<30K" name="test"></InputField>
+        <InputField handleChange={handleChange} value="50" title="<50K" name="test"></InputField>
+        <InputField handleChange={handleChange} value="80" title="<80K" name="test"></InputField>
+        <InputField handleChange={handleChange} value="100" title="<100K" name="test"></InputField>
+      </div>
     </div>
   )
 }
